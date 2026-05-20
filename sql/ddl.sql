acf7827be3822d9050cd6f696c05c4327dd4fe48
melisbm <miquelangel.melis@gmail.com>
2026-05-20 15:55:30 +0200

    add: sql ddl & game, genres and creator inserts

---
+++ sql/ddl.sql
@@ -0,0 +1,45 @@
+CREATE TABLE game(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(300) NOT NULL,
+    `description` TEXT,
+    price DECIMAL(15, 2) NOT NULL,
+    stock INT NOT NULL,
+    rating INT NOT NULL CHECK (rating BETWEEN 1 AND 5),
+    image_url TEXT
+);
+
+CREATE TABLE creator(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(200) NOT NULL,
+    last_name VARCHAR(150),
+    birth_date DATE,
+    country VARCHAR(200),
+    image_url TEXT
+);
+
+CREATE TABLE genre(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(200) NOT NULL,
+    `description` TEXT
+);
+
+CREATE TABLE game_genre(
+    game_id INT,
+    genre_id INT,
+
+    PRIMARY KEY (game_id, genre_id),
+
+    CONSTRAINT fk_game FOREIGN KEY (game_id) REFERENCES game(id),
+    CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genre(id)
+);
+
+CREATE TABLE game_creator(
+    game_id INT,
+    creator_id INT,
+    publish_date DATE NOT NULL,
+
+    PRIMARY KEY (game_id, creator_id),
+
+    CONSTRAINT fk_game FOREIGN KEY (game_id) REFERENCES game(id),
+    CONSTRAINT fk_creator FOREIGN KEY (creator_id) REFERENCES creator(id)
+);
---
+++ sql/genres_inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO genre (`name`, `description`)
+VALUES ('Horror', 'Games that are scary');
+INSERT INTO genre (`name`, `description`) VALUES ('Action', 'Fast-paced games focused on combat and reflexes.');
+INSERT INTO genre (`name`, `description`) VALUES ('RPG', 'Games where you develop a character through choices and progression.');
+INSERT INTO genre (`name`, `description`) VALUES ('Strategy', 'Games that require planning and tactical thinking to win.');
+INSERT INTO genre (`name`, `description`) VALUES ('Platformer', 'Games centered on jumping and navigating across levels.');
+INSERT INTO genre (`name`, `description`) VALUES ('Sports', 'Games simulating real or fictional competitive sports.');
+INSERT INTO genre (`name`, `description`) VALUES ('Simulation', 'Games that replicate real-world activities or systems.');
+INSERT INTO genre (`name`, `description`) VALUES ('Puzzle', 'Games that challenge the mind with logic and problem solving.');
+INSERT INTO genre (`name`, `description`) VALUES ('Adventure', 'Games focused on exploration and narrative-driven experiences.');
+INSERT INTO genre (`name`, `description`) VALUES ('Fighting', 'Games where players battle opponents in close combat.');

ea14c0b785fc734be2aab589f4ddebc4668a6413
melisbm <miquelangel.melis@gmail.com>
2026-05-20 17:02:14 +0200

    refactor: move server files to projecteVideojocs_server

---
+++ projecteVideojocs_server/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+      host: "db",
+      user: "daw",
+      password: "iesmanacor",
+      database: "practicavideojocs",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
